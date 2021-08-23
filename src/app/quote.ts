51fb049b8f6b47899468612c0f024f776eb270ad
mash <mashalonzo741@gmail.com>
2021-08-23 06:28:54 +0300

    Create the class blueprint

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,6 @@
 export class Quote {
-}
+    showDescription: boolean;
+    constructor(public id: number, public name: string, public author: string, public submitter: string, public completeDate: Date) {
+      this.showDescription = false;
+    }
+  }
