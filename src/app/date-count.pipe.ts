100a77703399d0febf7dc746f1c2eefff4fcfff7
mash <mashalonzo741@gmail.com>
2021-08-23 07:17:31 +0300

    Use the date inbuilt pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -4,9 +4,17 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'dateCount'
 })
 export class DateCountPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
-
-}
+    transform(value: any, args?: any): any {
+        if (value) {
+            const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+            if (seconds < 29) // less than 30 seconds ago will show as 'Just now'
+                return 'Just now';
+            const intervals: any = {
+                'year': 31536000,
+                'month': 2592000,
+                'week': 604800,
+                'day': 86400,
+                'hour': 3600,
+                'minute': 60,
+                'second': 1
+            };
