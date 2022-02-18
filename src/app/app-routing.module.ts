3a49e0a9c6fd3cfce2ed764e2b97e2bdc132dd6a
Mash14 <mashalonzo741@gmail.com>
2022-02-18 13:55:31 +0300

    Add 404 pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { QuoteComponent } from './quote/quote.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: QuoteComponent},
   { path: 'about', component: AboutComponent},
+  { path: 'quotes', redirectTo: '', pathMatch:'full'},
+  { path: '**', component: NotFoundComponent}, 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { QuoteService } from './quote-service/quote.service';
 import { AlertService } from './alert-service/alert.service';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     QuoteFormComponent,
     HighlightDirective,
     AboutComponent,
-    NavbarComponent
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
